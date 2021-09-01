8819e3ef7ca70d18aba622afd1ffa61d2faa758d
edilana <edilanacerqueira@gmail.com>
2021-09-01 15:04:42 -0300

    Inclusao de Dados Profissionais

--- src/main/java/br/com/edilana/cadastrocliente/resource/ClienteResource.java
+++ src/main/java/br/com/edilana/cadastrocliente/resource/ClienteResource.java
@@ -4,8 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.edilana.cadastrocliente.model.Cliente;
-
 @RestController
 public class ClienteResource {
 	 
